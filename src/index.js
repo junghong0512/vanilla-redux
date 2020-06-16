2e225c60962002c8ec7468f71885b1dd7360d2b5
Jung <junghong91@naver.com>
2020-06-16 18:28:43 +0900

    Adding a text, id object using spreading operator, not by mutation(push)

--- src/index.js
+++ src/index.js
@@ -40,22 +40,38 @@
 // minus.addEventListener("click", handleMinus);
 
 //////////////////////// For the ToDo List App ///////////////////////////////
+import { createStore } from "redux";
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (todo) => {
-  const li = document.createElement("li");
-  li.innerText = todo;
-  ul.appendChild(li);
+const reducer = (state = [], action) => {
+  console.log({ text: action.text });
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
 form.addEventListener("submit", onSubmit);
