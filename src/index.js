73cbf0dd829556a284a8392e2fae126bc930b78e
Jung <junghong91@naver.com>
2020-06-16 17:32:27 +0900

    Initial setting for the todo add, not using redux

--- src/index.js
+++ src/index.js
@@ -1,40 +1,61 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+// const add = document.getElementById("add");
+// const minus = document.getElementById("minus");
+// const number = document.querySelector("span");
 
-number.innerText = 0;
+// number.innerText = 0;
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+// const ADD = "ADD";
+// const MINUS = "MINUS";
 
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
+// const countModifier = (count = 0, action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return count + 1;
+//     case MINUS:
+//       return count - 1;
+//     default:
+//       return count;
+//   }
+// };
+
+// const countStore = createStore(countModifier);
+
+// const onChange = () => {
+//   number.innerText = countStore.getState();
+// };
+
+// countStore.subscribe(onChange);
+
+// const handleAdd = () => {
+//   countStore.dispatch({ type: ADD });
+// };
+
+// const handleMinus = () => {
+//   countStore.dispatch({ type: MINUS });
+// };
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
+
+//////////////////////// For the ToDo List App ///////////////////////////////
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const createToDo = (todo) => {
+  const li = document.createElement("li");
+  li.innerText = todo;
+  ul.appendChild(li);
 };
 
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
 };
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
