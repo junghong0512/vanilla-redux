76a8d975e839d704ab74b73fd7b631a3637b44d9
Jung <junghong91@naver.com>
2020-06-17 14:57:23 +0900

    reducer is updated by redux toolki createReducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // const ADD = "ADD";
 // const DELETE = "DELETE";
@@ -21,7 +21,7 @@ import { createAction } from "@reduxjs/toolkit";
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
+/* const reducer = (state = [], action) => {
   switch (action.type) {
     // case ADD:
     case addToDo.type:
@@ -34,7 +34,16 @@ const reducer = (state = [], action) => {
     default:
       return state;
   }
-};
+}; */
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => {
+    return state.filter((toDo) => toDo.id !== action.payload);
+  },
+});
 
 const store = createStore(reducer);
 
