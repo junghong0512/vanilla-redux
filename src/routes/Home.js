977039dc01f82c2c889de6f1c980827ca4700b59
Jung <junghong91@naver.com>
2020-06-17 12:15:29 +0900

    Connect Home to the store using connect()

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
 
   function onChange(e) {
@@ -19,9 +20,13 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>ADD</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return { toDos: state }; // 여기에서 return 한 obj 는 Home Compoenet에서 Prop에 저장된다
+}
+
+export default connect(mapStateToProps)(Home);
